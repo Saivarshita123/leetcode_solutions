06cb973288c86ff39ffe489b4b7f98d304f53efe
Sai varshitha.Guddanti <142594729+Saivarshita123@users.noreply.github.com>
2025-10-31 09:22:29 +0530

    Time: 3 ms (51.08%), Space: 17.8 MB (40.27%) - LeetHub

---
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        seen = set()
+        duplicates = []
+        for num in nums:
+            if num in seen:
+                duplicates.append(num)
+            else:
+                seen.add(num)
+        return duplicates
